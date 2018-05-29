c6e75430dc7c9caf94242f89ac93594253ea6221
dhanusya <dhanusyaksriet@gmail.com>
2018-05-29 15:33:13 +0530

    DataTypeExample

---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class DataTypeExample {
+     public static void main(String args[]){
+String firstname= "dhanusya";
+String lastname= "devi";
+System.out.println(firstname+""+lastname+"");
+}
+}
+
