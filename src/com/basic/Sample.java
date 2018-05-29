10961cb3a8728f0808e0e093c5c87af64dda338b
dhanusya <dhanusyaksriet@gmail.com>
2018-05-29 17:44:26 +0530

    correction

---
+++ src/com/basic/Sample.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+public class Sample {
+	public static void main(String args[]) {
+		int x=1,total=0;
+	while( x <= 100 )
+	{
+		total +=x;
+		++x;
+		System.out.println(total+"");
+	}
+}
+}
