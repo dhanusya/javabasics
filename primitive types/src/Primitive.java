5bf3d8178e6d280a9ea845692db4d6a640a1ee38
dhanusya <dhanusyaksriet@gmail.com>
2018-05-30 18:15:51 +0530

    primitive types example

---
+++ primitive types/src/Primitive.java
@@ -0,0 +1,21 @@
+
+public class Primitive {
+		  public static void main(String args[]) {
+		 byte b= 100;
+		 long l= 3545;
+		 short s= 456;
+		char ch= 'A';
+		boolean bh= true;
+		boolean br= false;
+		double d= 64775;
+		float f= 674.7594f;
+		System.out.println("byte="+b);
+		System.out.println("long="+l);
+		System.out.println("short="+s);
+		System.out.println("char="+ch);
+		System.out.println("boolean="+bh);
+		System.out.println("boolean="+bh);
+		System.out.println("double="+d);
+		System.out.println("float="+f);
+		}
+		}
