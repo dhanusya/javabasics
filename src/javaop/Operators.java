fe286978769b9e6e7ed2ff33eada5327d8f29963
dhanusya <dhanusyaksriet@gmail.com>
2018-05-30 12:51:13 +0530

    javaoperators

---
+++ src/javaop/Operators.java
@@ -0,0 +1,29 @@
+package javaop;
+
+public class Operators {
+	    public static void main(String args[]){
+		int a=3,b=4,c=3;
+		/*ARITHEMETIC OPERATORS*/
+		System.out.println("a+b"+a+b);
+		System.out.println("a+b"+a+c);
+		System.out.println("a*b"+a+b);
+		System.out.println("a%b"+a+b);
+		/*ASSIGNMENT OPERATORS*/
+		int x=34,y=12;
+		x=x+y;
+		y+=x;
+		System.out.println("x+ y+"+x+y);
+		/*RELATIONAL OPERATORS*/
+		int i=3;
+		if(i<4)
+		{
+			System.out.println(true);
+		}
+		else
+		{
+			System.out.println(false);
+		}
+	}
+
+
+}
