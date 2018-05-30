74079c347df73118cbfd7b3157032826cd7c53e3
dhanusya <dhanusyaksriet@gmail.com>
2018-05-30 18:04:13 +0530

    strings example

---
+++ java strings/src/Strings.java
@@ -0,0 +1,11 @@
+
+public class Strings {
+	public static void main(String args[]) {
+		char ch= 'S';
+		String s="this is the sample string";
+		System.out.println(ch+ch);
+		int len=s.length();
+		System.out.println("stringlength" +len);		
+	}
+
+}
