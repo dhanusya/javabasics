045dbac4637fd0fd1d615115b5d55c96fe5bede0
dhanusya <dhanusyaksriet@gmail.com>
2018-05-30 18:23:18 +0530

    sample

---
+++ Sample/src/Project.java
@@ -0,0 +1,13 @@
+
+public class Project {
+		  public static void main(String args[])
+		{
+		 int x = 1, total=0;
+		while ( x <= 10 )
+		{
+		total += x;
+		++x;
+		System.out.println(total+"");
+		}
+		}
+		}
