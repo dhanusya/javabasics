61ca6364f827a082d3e155f1d5cc011a21ee2529
dhanusya <dhanusyaksriet@gmail.com>
2018-05-31 14:40:26 +0530

    invoice example

---
+++ invoice example/src/Admin.java
@@ -0,0 +1,12 @@
+
+public class Admin {
+	public static void main(String args[]){	
+	Invoice Invoice1=new Invoice();
+	Invoice1.setQuantity(5);
+	Invoice1.setPrice(100);
+	Invoice1.setPartDescription("Bold");
+	System.out.println(Invoice1.getQuantity());
+	System.out.println(Invoice1.getInvoiceAmount());
+	System.out.println(Invoice1.getPartDescription());
+	}
+}
