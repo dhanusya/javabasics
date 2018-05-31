46a365c843511725958701da214ed201eb153ef8
dhanusya <dhanusyaksriet@gmail.com>
2018-05-31 15:12:13 +0530

    Date example

---
+++ Data example/src/Month.java
@@ -0,0 +1,13 @@
+
+public class Month {
+public static void main(String args[])
+{
+	Date d1=new Date();
+	d1.setDay(31);
+	d1.setMonth(5);
+	d1.setYear(2018);
+	d1.displayDate();
+	//Date d2= new Date(2,2018,4);
+	//d2.displayDate();
+}
+}
