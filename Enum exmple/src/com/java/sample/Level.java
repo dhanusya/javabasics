002a26da0f12eeb9fbaf3214428235eb300527af
dhanusya <dhanusyaksriet@gmail.com>
2018-06-10 11:29:56 +0530

    Enum example

---
+++ Enum exmple/src/com/java/sample/Level.java
@@ -0,0 +1,14 @@
+package com.java.sample;
+
+public enum Level {
+	Mercury(10),
+	Venus(3),
+	Earth(6);
+	int value;
+	private Level(int value){
+		this.value=value;
+			
+		}
+		
+	}
+
---
+++ Enum exmple/src/com/java/sample/Sample.java
@@ -0,0 +1,12 @@
+package com.java.sample;
+
+public class Sample {
+	public static void main(String args[]){
+		Level lev=Level.Mercury;
+		System.out.println(lev);
+		System.out.println(lev.name());
+		System.out.println(lev.ordinal());
+		System.out.println(lev.ordinal());
+		
+}
+}
